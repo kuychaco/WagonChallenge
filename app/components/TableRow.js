d5edcab673a40ef2a605182abfc2553098583a1c
Katrina Uychaco <kuychaco@gmail.com>
2015-10-08 17:52:56 -0700

    Fix bug for filter

--- app/components/TableRow.js
+++ app/components/TableRow.js
@@ -6,7 +6,7 @@ var TableRow = React.createClass({
   render () {
     var data = this.props.data;
     var contents = data.map((value, index) => {
-      return (<td key={value+index}> {value} </td>);
+      return (<td> {value} </td>);
     });
     return (<tr> {contents} </tr>);
   }
